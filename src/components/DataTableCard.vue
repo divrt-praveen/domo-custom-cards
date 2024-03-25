ea19f42cb4c69b232bc7b70e632e8e9730783f34
divrt-praveen <pm0@spplus.com>
2024-03-25 23:24:30 +0530

    single card with two components

---
+++ src/components/DataTableCard.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-container>
+      <v-row class="text-center">
+        <v-col cols="12">
+          <v-data-table :headers="headers" :items="getItems" :loading="loading"></v-data-table>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+  
+  <script>
+  export default {
+    name: "HelloWorld",
+  
+    data: () => ({
+      loading: false,
+      reportData: [],
+      headers: [
+        { text: "Exit Month Label", value: "exitMonthLabel" },
+        { text: "Booking Id", value: "bid" },
+        { text: "Booking State Label", value: "bookingStateLabel" },
+        { text: "Entry time", value: "entryTime" },
+        { text: "Exit time", value: "exitTime" },
+      ],
+    }),
+    computed:{
+      getItems(){
+        return this.reportData;
+      }
+    },
+    mounted() {
+      this.loading = false;
+      this.fetchData();
+    },
+    methods: {
+      async fetchData() {
+        this.loading = true;
+  
+       this.reportData =  await window.domo
+          .get("/data/v2/durationReportData?limit=100&filter=zcode==99999")
+          // .then(function (durationReportData) {
+          //   // this.reportData = durationReportData;
+          //   // console.log("durationReportData", durationReportData);
+          // });
+        this.loading = false;
+      },
+    },
+  };
+  </script>
+  
---
+++ src/components/MonthPickerCard.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="hello">
+      <v-row no-gutters>
+        <v-col cols="12">
+          <v-menu
+            ref="monthPickerMenu"
+            v-model="monthPickerMenu"
+            :close-on-content-click="false"
+            :return-value.sync="monthSelected"
+            transition="scale-transition"
+            offset-y
+            width="416.75px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-row
+                no-gutters
+                class="fill-height mb-2"
+                style="border: 1px solid #979797"
+                v-on="on"
+                v-bind="attrs"
+              >
+                <v-col cols="10">
+                  <v-row no-gutters>
+                    <v-col
+                      cols="12"
+                      class="text-start px-2 pt-2 font-weight-bold"
+                    >
+                      MONTH
+                    </v-col>
+                    <v-col
+                      cols="12"
+                      class="text-start px-2 black--text font-weight-bold"
+                    >
+                      {{ monthSelected }}
+                    </v-col>
+                  </v-row>
+                </v-col>
+                <v-col cols="2" class="align-self-center">
+                  <v-icon color="primary">{{
+                    monthPickerMenu ? "mdi-menu-up" : "mdi-menu-down"
+                  }}</v-icon>
+                </v-col>
+              </v-row>
+            </template>
+            <v-date-picker
+              width="402px"
+              v-model="monthSelected"
+              :max="maxDate"
+              type="month"
+              no-title
+              scrollable
+              @change="setMonthPicker()"
+            >
+            </v-date-picker>
+          </v-menu>
+        </v-col>
+      </v-row>
+    </div>
+  </template>
+  
+  <script>
+  import { format } from "date-fns";
+  export default {
+    name: "HelloWorld",
+    props: {
+      msg: String,
+    },
+    data() {
+      return {
+        monthPickerMenu: false,
+        monthSelected: new Date().toISOString().substr(0, 7),
+        maxDate: new Date().toISOString().substr(0, 7),
+      };
+    },
+    methods: {
+      async setMonthPicker() {
+        this.$refs.monthPickerMenu.save(this.monthSelected);
+        await window.domo.filterContainer([
+          {
+            column: "exitMonthLabel",
+            operator: "IN",
+            values: [format(new Date(this.monthSelected), "MMMM yyyy")],
+            dataType: "STRING",
+          },
+        ]);
+        // window.domo.onFiltersUpdate(console.log);
+  
+      },
+    },
+  };
+  </script>
