eceb624f94612d5afe637f51dd6b00860251c4cd
divrt-praveen <pm0@spplus.com>
2024-03-25 22:36:40 +0530

    Month Picker Filter

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,151 +1,91 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
+  <div class="hello">
+    <v-row no-gutters>
       <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
+        <v-menu
+          ref="monthPickerMenu"
+          v-model="monthPickerMenu"
+          :close-on-content-click="false"
+          :return-value.sync="monthSelected"
+          transition="scale-transition"
+          offset-y
+          width="416.75px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-row
+              no-gutters
+              class="fill-height mb-2"
+              style="border: 1px solid #979797"
+              v-on="on"
+              v-bind="attrs"
+            >
+              <v-col cols="10">
+                <v-row no-gutters>
+                  <v-col
+                    cols="12"
+                    class="text-start px-2 pt-2 font-weight-bold"
+                  >
+                    MONTH
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    class="text-start px-2 black--text font-weight-bold"
+                  >
+                    {{ monthSelected }}
+                  </v-col>
+                </v-row>
+              </v-col>
+              <v-col cols="2" class="align-self-center">
+                <v-icon color="primary">{{
+                  monthPickerMenu ? "mdi-menu-up" : "mdi-menu-down"
+                }}</v-icon>
+              </v-col>
+            </v-row>
+          </template>
+          <v-date-picker
+            width="402px"
+            v-model="monthSelected"
+            :max="maxDate"
+            type="month"
+            no-title
+            scrollable
+            @change="setMonthPicker()"
           >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
+          </v-date-picker>
+        </v-menu>
       </v-col>
     </v-row>
-  </v-container>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
+import { format } from "date-fns";
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      monthPickerMenu: false,
+      monthSelected: new Date().toISOString().substr(0, 7),
+      maxDate: new Date().toISOString().substr(0, 7),
+    };
+  },
+  methods: {
+    async setMonthPicker() {
+      this.$refs.monthPickerMenu.save(this.monthSelected);
+      await window.domo.filterContainer([
+        {
+          column: "exitMonthLabel",
+          operator: "IN",
+          values: [format(new Date(this.monthSelected), "MMMM yyyy")],
+          dataType: "STRING",
+        },
+      ]);
+      window.domo.onFiltersUpdate(console.log);
 
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
-    }),
-  }
+    },
+  },
+};
 </script>
